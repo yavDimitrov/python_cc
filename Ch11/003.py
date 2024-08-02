d4cd19637fd1a15edca32bbf1d5cdaf66e54d6c1
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-02 16:09:26 +0300

    ch11 end

---
+++ Ch11/003.py
@@ -0,0 +1,9 @@
+my_name = "Yavor"
+my_hobby = "cycling"
+time = 8
+
+info = f"{my_name} likes {my_hobby} at {time} o'clock"
+print(info)
+
+info01 = '{} likes {} at {} o\'clock'.format(my_name, my_hobby, time)
+print(info01)
