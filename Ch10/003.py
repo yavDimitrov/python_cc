91fd9336e547a24081a2a9ae6d40a4bfcb289557
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-02 15:02:37 +0300

    ch 10 end

---
+++ Ch10/003.py
@@ -0,0 +1,8 @@
+my_comment = "this is my short comment"
+print(my_comment)
+print(len(my_comment))
+
+my_comment = my_comment.replace('short', 'long')
+
+print(my_comment)
+print(len(my_comment))
---
+++ Ch10/003_2.py
@@ -0,0 +1,9 @@
+my_comment = "this is my short comment"
+
+print(my_comment.count('is'))
+print(my_comment[0:-3])
+print(my_comment.find('short'))
+print(my_comment[11:])
+
+print(my_comment.find('short') == -1)
+print(dir(my_comment))
