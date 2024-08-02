03a6f6bdfa4cd6fc68705b597cbf978976c4fe10
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-02 14:37:12 +0300

    test

--- Ch10/002_2.py
+++ Ch10/002_2.py
@@ -1,6 +1,16 @@
-long_str = "This is a very long string!We can escape symbols with \""
+long_str = "This is a very long string!" \
+           "We can escape symbols with \""
+
+long_str2 = """This is a very
+            long string!
+            We can escape symbols with \""""
 
 print(long_str)
 print(type(long_str))
 print(type(long_str) == str)
 print(id(long_str))
+
+print(long_str2)
+print(type(long_str2))
+print(type(long_str2) == str)
+print(id(long_str2))
