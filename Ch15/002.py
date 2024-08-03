de2f6aee16cbf26fa82e3fdb1e8f431662f36127
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-03 15:11:36 +0300

    ch15 start

---
+++ Ch15/002.py
@@ -0,0 +1,14 @@
+my_ratings = [2.5, 5.0]
+other_ratings = [3.7, 4.5, 4.9]
+
+all_ratings = my_ratings + other_ratings
+print(all_ratings)
+
+first_two_ratings = all_ratings[:2]
+print(first_two_ratings)
+
+middle_ratings = all_ratings[1:-1]
+print(middle_ratings)
+
+last_two_ratings = all_ratings[-2:]
+print(last_two_ratings)
