b5c97e942bf86b6acbae33e796cfcb593da592b4
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-04 13:11:59 +0300

    ch16 end

---
+++ Ch16_Dictionary/005.py
@@ -0,0 +1,3 @@
+my_bike = {'brand': 'CROSS', 'price': 1300, 'colour': 'blue'}
+
+print(my_bike.get('colour', 'black'))
---
+++ Ch16_Dictionary/006.py
@@ -0,0 +1,10 @@
+my_list = [['a', 10], ['b', 20]]
+
+my_dict = dict(my_list)
+
+my_tapl = [('a', 10), ('b', 20)]
+
+my_tapl_to_dict = dict(my_tapl)
+
+print(my_dict)
+print(my_tapl_to_dict)
