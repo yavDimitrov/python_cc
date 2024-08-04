23a57030d2d0fab9a2c62739fb375f2fbb4d2f8b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-04 12:38:14 +0300

    ch16

---
+++ Ch16/002.py
@@ -0,0 +1,11 @@
+my_bike = {'brand': 'CROSS', 'price': 1300}
+
+print(my_bike)
+print(type(my_bike))
+print(len(my_bike))
+
+print(my_bike['brand'])
+print(my_bike['price'])
+
+my_bike['max_speed'] = 68
+print(my_bike)
---
+++ Ch16/002_2.py
@@ -0,0 +1,6 @@
+my_bike = {'brand': 'CROSS', 'price': 1300}
+
+other_bike = {'price': 1300, 'brand': 'CROSS'}
+
+print(my_bike == other_bike)
+print(id(my_bike) == id(other_bike))
