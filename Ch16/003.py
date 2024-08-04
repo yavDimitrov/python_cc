3e30274c31030b7b71c4a5d82d153986dd8a1ada
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-04 12:50:56 +0300

    ch16 002

---
+++ Ch16/003.py
@@ -0,0 +1,12 @@
+my_bike = {'brand': 'CROSS', 'price': 1300}
+
+keys = my_bike.keys()
+
+print(keys)
+print(type(keys))
+
+n_keys = my_bike.items()
+n_keys2 = list(my_bike.items())
+
+print(n_keys)
+print(n_keys2)
---
+++ Ch16/003_2.py
@@ -0,0 +1,7 @@
+my_bike = {'brand': 'CROSS', 'price': 1300}
+
+other_bike = my_bike.copy()
+
+other_bike['colour'] = 'blue'
+print(my_bike)
+print(other_bike)
