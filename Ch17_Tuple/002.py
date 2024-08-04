af3d0914275f1511349d72d844d8eb0567f2d0d2
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-04 17:19:13 +0300

    ch17

---
+++ Ch17_Tuple/002.py
@@ -0,0 +1,9 @@
+screen_res = (1920, 1080, 1080)
+
+print(screen_res)
+print(type(screen_res))
+print(isinstance(screen_res, tuple))
+
+print(screen_res.index(1080))
+print(screen_res.count(1080))
+
---
+++ Ch17_Tuple/003.py
@@ -0,0 +1,12 @@
+screen_res = (1920, 1080, 1080)
+screen_info = (True, 'Samsung', 150)
+
+screen_data = screen_info + screen_res
+
+print(screen_data)
+print(screen_res)
+print(screen_info)
+
+print(screen_data[0])
+print(screen_data[-1])
+print(len(screen_data))
