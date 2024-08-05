e2bf7d1e55ce0b1e47d5d7e4c5958c9632cc1f47
Yavor Dimitrov <yav.dimitrov@gmail.com>
2024-08-05 11:48:39 +0300

    ch18

---
+++ Ch18_Set/002.py
@@ -0,0 +1,6 @@
+my_set = {100, 10, 5, 5, 20}
+
+print(my_set)
+print(type(my_set))
+print(isinstance(my_set, set))
+print(len(my_set))
---
+++ Ch18_Set/002_2.py
@@ -0,0 +1,6 @@
+my_set = {100, True, 5, 'abcd', 20}
+print(my_set)
+
+new_set = set()
+print(new_set)
+print(type(new_set))
